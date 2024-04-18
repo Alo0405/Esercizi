1687a66e88ff7746164395a87636cd527f3c3b72
Alo0405 <valeriorondoni0504@gmail.com>
2024-04-18 10:24:40 +0200

    secondo upload esercizio

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -3,11 +3,72 @@
 
 #print ("hello world")
 
-'''2-3. Personal Message: Use a variable to represent a person’s name, and print
- a message to that person. Your message should be simple, such 
-as, “Hello Eric, would you like to learn some Python today?”'''
+#2-3. Personal Message: Use a variable to represent a person’s name, and print
+#a message to that person. Your message should be simple, such 
+#as, “Hello Eric, would you like to learn some Python today?”'''
 
 
-name = 'Mario'
+'''name = 'Mario'
 message: str = f'ciao {name}, ti va imparare un pò di python insieme'
-print(message)
+print(message)'''
+
+
+#2-4. Name Cases: Use a variable to represent
+#a person’s name, and then print
+#that person’s name in lowercase, uppercase, and title case.'
+
+
+'''name: str = 'Valerio'
+name_lower: str = name.lower()
+print(name_lower)
+name_upper: str = name.upper()
+print(name_upper)
+name_title: str = name.title()
+print(name_title)'''
+
+
+#2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote
+#and the name of its author. Your output
+#should look something like the following, including the quotation marks: Albert Einstein
+#once said, “A person who never
+#made a mistake never tried anything new.”
+
+
+'print(Albert Einstein once said: “A person who never made a mistake never tried anything new.”)'
+
+
+#2-6
+
+'''famous_person: str = 'Albert Einstein'
+print(f'{famous_person} once said: “A person who never made a mistake never tried anything new.”')'''
+
+
+#2-8
+
+
+'''filename: str = 'python_notes.txt'
+print('python_notes.txt'.removesuffix('.txt'))'''
+
+
+#3-1
+
+
+'''amici_list: list = ['Federico', 'Leonardo', 'Giulio', 'Bruno']
+print(amici_list[0])
+print(amici_list[1])
+print(amici_list[2])
+print(amici_list[3])'''
+
+
+#3-2
+
+
+'''amici_list: list = ['Federico', 'Leonardo', 'Giulio', 'Bruno']
+message: str ='ciao valerio'
+print(f'{amici_list[0]} ha detto {message}')
+print(f'{amici_list[1]} ha detto {message}')
+print(f'{amici_list[2]} ha detto {message}')
+print(f'{amici_list[3]} ha detto {message}')'''
+
+
+#3-3
