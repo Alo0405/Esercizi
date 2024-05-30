d3b93e66ef92c57e08d1e9b18ba439131c3c3e0a
Alo0405 <valeriorondoni0504@gmail.com>
2024-05-30 13:45:34 +0200

    update

--- Lezione12/Lezione12.py
+++ Lezione12/Lezione12.py
@@ -1,4 +1,6 @@
-class Libro:
+#Libreria
+
+'''class Libro:
     def __init__(self, titolo: str, autore: str) -> None:
         self.titolo = titolo
         self.autore = autore
@@ -7,7 +9,7 @@ class Biblioteca:
     def __init__(self) -> None:
         self.books = []
     def add_books(self, book: Libro):
-        self.book = book
+        self.book = book 
         self.books.append(book)
         print('Il libro è stato aggiunto')
     def lend_books(self, titolo: str):
@@ -41,7 +43,57 @@ book2: Libro = Libro ('ololo', 'Gabriel')
 Biblioteca1: Biblioteca = Biblioteca()
 Biblioteca1.add_books(book1)
 Biblioteca1.lend_books(book2)
-Biblioteca1.show_books()
+Biblioteca1.show_books()'''
+
+class MovieCatalog:
+
+    def __init__(self) -> None:
+        
+        self.movies = {}
+
+    def add_movie(self, director_name: str, movies: list):
+
+        for movie in movies:
+            if (director_name in self.movies):
+                self.movies[director_name].append(movie)
+            else:               
+                self.movies.update({director_name: [movie]})
+
+    def remove_movie(self, director_name: str, movie: str):
+
+        self.movies[director_name].remove(movie)
+        if (self.movies[director_name] == []):
+            print('The director has no more movies in the catalog do you want to remove it too? Say y or n: ', end = '')
+            choice: str = input()
+            if (choice == 'y'):
+                self.movies.pop(director_name)
+
+    def list_directors(self):
+        
+        print('Directors: ')
+        for director in self.movies:
+            print(director)
+
+    def get_movies(self, director_name: str):
+
+        print('Movies:')
+        for movie in self.movies[director_name]:
+                print(movie)
+    
+    def search_movies_by_title(self):
+
+        pass
+
+
+
+catalog1: MovieCatalog = MovieCatalog()
+catalog1.add_movie('Valerio', ['Harry Potter: La pietra filosofale'])
+catalog1.add_movie('Valerio', ['Harry Potter: L\'ordine della fenice'])
+catalog1.remove_movie('Valerio', 'Harry Potter: La pietra filosofale')
+catalog1.list_directors()
+catalog1.get_movies('Valerio')
+catalog1.remove_movie('Valerio', 'Harry Potter: L\'ordine della fenice')
+catalog1.list_directors()
 
 
 
--- Lezione9/Lezione9.py
+++ Lezione9/Lezione9.py
@@ -85,3 +85,122 @@ def valid_sudoku(board: list[list[str]]) -> bool:
 
 def anagram(s: str, t: str) -> bool:
     return sorted(s.lower()) == sorted(t.lower())
+
+
+
+
+
+
+
+#5
+
+class Book:
+    
+    def __init__(self, book_id: str, title: str, author: str) -> None:
+        
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.is_borrowed = False
+    
+    def borrow(self):
+        
+        self.is_borrowed = True
+    
+    def return_book(self):
+        
+        self.is_borrowed = False
+
+class Member:
+    
+    def __init__(self, member_id: str, name: str) -> None:
+
+        self.member_id = member_id
+        self.name = name
+        self.borrowed_books = []
+
+    def borrow_book(self, book: Book):
+
+        if (book in self.borrowed_books):
+            pass
+        else: 
+            self.borrowed_books.append(book)
+            book.borrow()
+
+    def return_book(self, book: Book):
+
+        if (book in self.borrowed_books):   
+            self.borrowed_books.remove(book)
+            book.return_book()
+        
+class Library:
+    
+    def __init__(self) -> None:
+
+        self.books = {}
+        self.members = {}
+    
+    def add_book(self, book_id: str, title: str, author: str):
+
+        book: Book = Book(book_id, title, author)
+        self.books.setdefault(book_id, book)
+
+    def register_member(self, member_id: str, name: str):
+
+        member: Member = Member(member_id, name)
+        self.members.setdefault(member_id, member)
+
+    def borrow_book(self, member_id: str, book_id: str):
+
+        self.members[member_id].borrow_book(self.books[book_id])
+    
+    def return_book(self, member_id: str, book_id: str):
+
+        self.members[member_id].return_book(self.books[book_id])
+    
+    def get_borrowed_books(self, member_id: str) -> list:
+
+        return self.members[member_id].borrowed_books
+
+library = Library()
+
+library.add_book("B001", "The Great Gatsby", "F. Scott Fitzgerald")
+library.add_book("B002", "1984", "George Orwell")
+library.add_book("B003", "To Kill a Mockingbird", "Harper Lee")
+
+# Register members
+library.register_member("M001", "Alice")
+library.register_member("M002", "Bob")
+library.register_member("M003", "Charlie")
+
+# Borrow books
+library.borrow_book("M001", "B001")
+library.borrow_book("M002", "B002")
+
+print(library.get_borrowed_books("M001"))  # Expected output: ['The Great Gatsby']
+print(library.get_borrowed_books("M002"))  # Expected output: ['1984']
+
+#6
+
+class Account:
+    def __init__(self, account_id: str)->None:
+        self.account_id = account_id
+        self.balance = 0
+    def deposit(self, amount: float)->None:
+        self.balance = self.balance + amount 
+    def get_balance(self):
+        return self.balance
+
+class Bank:
+    def __init__(self)->None:
+        self.accounts = {}
+    def create_account(self, account_id)->None:
+        
+        account: Account = Account (account_id)
+        self.accounts.update({account_id: account})
+        return account
+        
+    def deposit(self, account_id, amount)-> None:
+        self.accounts[account_id].deposit(amount)
+    def get_balance(self, account_id):   
+        self.accounts[account_id].get_balance()
